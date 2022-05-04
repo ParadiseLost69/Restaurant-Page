5381b1cbf331c4e1ca8e797aceb88e864d02072b
Teddy <tedloukas@outlook.com>
2022-05-04 20:11:43 +0900

    Finish menu and add some styling

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,7 @@
 import coffeeImage from "./Images/CoffeeMenu.jpg";
 import espressoImage from "./Images/espresso.jpeg";
+import latteImage from "./Images/Latte.jpg";
+import cappuccinoImage from "./Images/Cappuccino.jpg";
 
 const menu = function () {
   //create Menu DIV
@@ -20,7 +22,9 @@ const menu = function () {
   const coffee = document.createElement("div");
   coffee.className = "card";
   const cappuccino = document.createElement("div");
+  cappuccino.className = "card";
   const latte = document.createElement("div");
+  latte.className = "card";
 
   //INFORMATION FOR ESPRESSO
   let espressoPic = new Image();
@@ -40,18 +44,44 @@ const menu = function () {
   const coName = document.createElement("h2");
   const coPrice = document.createElement("p");
 
-  coName.textContent = "Coffee";
+  coName.textContent = "Hand-drip Coffee";
   coPrice.textContent = "$22.99";
 
   coffee.appendChild(coffeePic);
   coffee.appendChild(coName);
   coffee.appendChild(coPrice);
 
+  //INFORMATION FOR LATTE
+  let lattePic = new Image();
+  lattePic.src = latteImage;
+  const laName = document.createElement("h2");
+  const laPrice = document.createElement("p");
+
+  laName.textContent = "Cafè Latte";
+  laPrice.textContent = "$30.99";
+
+  latte.appendChild(lattePic);
+  latte.appendChild(laName);
+  latte.appendChild(laPrice);
+
   //INFORMATION FOR CAPPUCCINO
+  const cappuccinoPic = new Image();
+  cappuccinoPic.src = cappuccinoImage;
+  const caName = document.createElement("h2");
+  const caPrice = document.createElement("p");
+
+  caName.textContent = "Cappuccino";
+  caPrice.textContent = "$19.99";
+
+  cappuccino.appendChild(cappuccinoPic);
+  cappuccino.appendChild(caName);
+  cappuccino.appendChild(caPrice);
 
   //appending to menu
   menuBook.appendChild(espresso);
   menuBook.appendChild(coffee);
+  menuBook.appendChild(latte);
+  menuBook.appendChild(cappuccino);
 };
 
 const menuItem = (name, price, description) => {
